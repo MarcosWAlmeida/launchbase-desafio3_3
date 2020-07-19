4512f24750ff265e8495d0572ff0eb067f2dc619
MarcosWAlmeida <marcos.sor@gmail.com>
2020-07-19 17:08:25 -0300

    finalização da rota req.params

--- assets/script.js
+++ assets/script.js
@@ -5,35 +5,15 @@ const cursos = document.querySelectorAll('.curso')
 for (let curso of cursos) {
     curso.addEventListener("click", function(){
         const get_id = curso.getAttribute("id")
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src = ` https://rocketseat.com.br/${get_id}`
+        window.location.href = `/courses/${get_id}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener("click" , function(){
-    modalOverlay.classList.remove('active')
-})
-
-document.querySelector('.close-modal').addEventListener("click" , function(){
-    modalMax.classList.remove('maximize')
-})
-
-window.onclick = function(event) {
-    if (event.target == modalOverlay) {
-        modalOverlay.classList.remove('active')
-    }
-}
 
 
 
-document.querySelector('.maximize-modal').addEventListener("click", function(){
-    const contain = modalMax.classList.contains('maximize')
-    if (!contain) {
-        modalMax.classList.add('maximize')
-    } else {
-        modalMax.classList.remove('maximize')
-    }
-})
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-const videos = require("./data")
+const courses = require("./data")
 
 server.use(express.static('assets'))
 
@@ -17,11 +17,28 @@ server.get("/", function(req, res ) {
 })
 
 server.get("/conteudo", function(req, res ) {
-    return res.render("conteudo" , {items: videos})
+    return res.render("conteudo" , {items: courses})
 })
 
 server.get("/sobre", function(req, res ) {
     return res.render("sobre")
+})
+
+server.get("/courses", function(req, res ) {
+    return res.render("courses")
+})
+
+server.get("/courses/:id" , function(req , res){
+    const id = req.params.id;
+   
+    const course = courses.find(function(course){
+        return course.id == id
+    })
+
+    if(!course) {
+        return res.send("Course not found!")
+    }
+     return res.render("courses", {items: course})
 })
 
 server.use(function(req, res) {
